a2b548fd063c3d2c1b9e420245133acf9adfb141
harrisonarmstrong93@gmail.com <harrisonarmstrong93@gmail.com>
2021-08-26 13:37:41 -0400

    Added Sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { SideNavItems, SideNavLink } from 'carbon-components-react/lib/components/UIShell';
+
+import { StyledSideNav } from './styles';
+
+const items = [
+  { name: 'Me', path: '/personal-portfolio/' },
+  { name: 'Projects', path: '/personal-portoflio/projects' },
+  { name: 'Work', path: '/personal-portoflio/work' },
+  { name: 'Education', path: '/personal-portfolio/education/' },
+];
+
+const Sidebar = () => {
+  const location = useLocation();
+
+  return (
+    <StyledSideNav isFixedNav expanded isChildOfHeader={false} aria-label="Side navigation">
+      <SideNavItems>
+        {items.map(i => (
+          <SideNavLink
+            isActive={
+              location.pathname === '/' && i.path === '/' ? true : location.pathname === i.path
+            }
+            element={Link}
+            to={i.path}
+            key={i.name}
+          >
+            {i.name}
+          </SideNavLink>
+        ))}
+      </SideNavItems>
+    </StyledSideNav>
+  );
+};
+
+export default Sidebar;
